6e7799550987490e5cbfe7f1e54b039327f36a94
yoon <cbj0010@naver.com>
2024-08-15 22:10:14 +0900

    책 코드 작성

--- Cafe/src/model/Menu.java
+++ Cafe/src/model/Menu.java
@@ -1,4 +1,24 @@
 package model;
 
+import java.util.List;
+
+/**
+ * 이 친구는 menuName에 해당하는 MenuItem을 찾아야하는 책임씨가 있다.
+ * Menu가 내부적으로 MenuItem을 관리하고 있어야한다.
+ */
 public class Menu {
+    private List<MenuItem> menuList;
+
+    public Menu(List<MenuItem> menuList) {
+        this.menuList = menuList;
+    }
+
+    public MenuItem choose(String menuName) {   //MenuItem에 입력받은 이름이 있는지 검사
+        for (MenuItem item : menuList) {
+            if (item.getMenuName().equals(menuName)) {
+                return item;
+            }
+        }
+        return null;
+    }
 }
---
+++ Cafe/src/model/MenuItem.java
@@ -0,0 +1,18 @@
+package model;
+
+public class MenuItem {
+    private String menuName;
+    private int price;
+
+    public MenuItem(String menuName, int price) {
+        this.menuName = menuName;
+        this.price = price;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public int cost() {
+    }
+}
